a4ca459bf27670a16226fb42eee54b835e6504ec
Syed Muzammil hussain <muzipak130@gmail.com>
2025-07-20 19:31:25 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+function glass(flavour){
+    console.log("My function is created!");
+    var num = 6;
+    var num2 = 3;
+    // alert(num + num2);
+    console.log(flavour + num + num2);
+
+}
+
+function name (bob){
+    console.log("hello there " +bob);
+}
+
